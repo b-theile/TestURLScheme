ef2a1338fc70b79d1b0a28345bf687a9048423f7
p.schiwek <p.schiwek@mru.de>
2016-11-04 14:30:38 +0100

    Intent wird jetzt von MainActivity gestartet, danach wird Finish aufgerufen.

--- TestURLScheme/TestURLScheme.Droid/MainActivity.cs
+++ TestURLScheme/TestURLScheme.Droid/MainActivity.cs
@@ -7,6 +7,8 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Xamarin.Forms;
+using Android.Content;
+using Android.Net;
 
 namespace TestURLScheme.Droid
 {
@@ -30,6 +32,15 @@ namespace TestURLScheme.Droid
             {
                 MessagingCenter.Send<string>(Intent.Data.EncodedPath, "mru4uresponse");
             }
+
+            MessagingCenter.Subscribe<string>(this, "mru4urequest", (urlString) => 
+            {
+                Intent intent = new Intent(Intent.ActionView, Android.Net.Uri.Parse(urlString));
+                //Intent intent = new Intent(Intent.ActionSend, Android.Net.Uri.Parse(urlString));
+                //Forms.Context.StartActivity(Intent.CreateChooser(intent, "Messungen bereitstellen"));
+                Forms.Context.StartActivity(intent);
+                this.Finish();
+            });
         }
     }
 }

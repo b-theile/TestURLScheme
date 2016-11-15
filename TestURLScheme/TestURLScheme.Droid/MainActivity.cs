bd310262241c1c3a6f4dbcc5d34b59b52957b900
Oliver Theile <o.theile@mru.de>
2016-11-15 13:03:56 +0100

    * MainActivity as SingleTask

--- TestURLScheme/TestURLScheme.Droid/MainActivity.cs
+++ TestURLScheme/TestURLScheme.Droid/MainActivity.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 namespace TestURLScheme.Droid
 {
-    [Activity(Label = "TestURLScheme", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, LaunchMode =LaunchMode.SingleTop)]
-    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+    [Activity(Label = "TestURLScheme", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, LaunchMode =LaunchMode.SingleTask)]
+
+    [IntentFilter( new[] { Intent.ActionView },
         DataScheme = "mru4uresponse",
-        Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable })]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+        Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable })]
+
+    public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
@@ -23,13 +27,22 @@ namespace TestURLScheme.Droid
 
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Forms.Init(this, bundle);
             LoadApplication(new App());
 
             if (Intent.Data != null)
             {
                 MessagingCenter.Send<string>(Intent.Data.EncodedPath, "mru4uresponse");
             }
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            if (intent.Data != null)
+            {
+                MessagingCenter.Send<string>(intent.Data.EncodedPath, "mru4uresponse");
+            }
+            base.OnNewIntent(intent);
         }
     }
 }
